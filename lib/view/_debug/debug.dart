8d0b1cfc0cbd04220afbaeeca7fa886d03bc1129
Deeny <5lineofcode@gmail.com>
2019-06-26 15:30:21 +0700

    Fixed Location Picker, UPgraded, Need Marker

--- lib/view/_debug/debug.dart
+++ lib/view/_debug/debug.dart
@@ -39,12 +39,23 @@ class _DebugPageState extends State<DebugPage> {
               lng: 0,
               placeId: "",
             ),
+            Text(Input.get("placeId") == null
+                ? ""
+                : Input.get("placeId").toString()),
+            Text(Input.get("lat") == null ? "" : Input.get("lat").toString()),
+            Text(Input.get("lng") == null ? "" : Input.get("lng").toString()),
             ExButton(
               label: "GetLocation",
               icon: Icons.timeline,
               type: ButtonType.primary,
               onPressed: () {
-                Page.show(context, SwiperExamplePage());
+                var placeId = Input.get("placeId");
+                var lat = Input.get("lat");
+                var lng = Input.get("lng");
+
+                print("placeId : $placeId");
+                print("lat : $lat");
+                print("lng : $lng");
               },
             ),
           ],
