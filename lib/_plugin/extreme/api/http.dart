8df3049fc6bbd54215c961317ccd3485827ddb2d
Deeny <5lineofcode@gmail.com>
2019-07-01 08:52:35 +0700

    Pagi 1 July 2019

--- lib/_plugin/extreme/api/http.dart
+++ lib/_plugin/extreme/api/http.dart
@@ -1,54 +1,82 @@
 import 'dart:async';
 import 'package:extremecore/core.dart';
-import 'package:flutter/material.dart';
+
+var cachedEndpoint = [
+  "product",
+  "product_category",
+  "product_station",
+];
 
 class ExtremeHttp {
-  Completer completer = new Completer();
+  Future<dynamic> get(String url) async {
 
-  final GlobalKey<NavigatorState> navigatorKey =
-      new GlobalKey<NavigatorState>();
+    /*
+    TODO: Check if endpoint need update or not, if not just return sqlite Value
+    TODO: Use Model and Cached it on Sqlite at Client Side, and New Database Called EndpointStatus to Save All Endpoint Version
+    TODO: if Cached Endpoint Version is Different From Endpoint Status, set CachedModel needUpdate field to true
+    TODO: if not, just return value from sqlite
+    TODO: if no endpoint found on the model, do normal http Request and Response
+    */
 
-  Future get(String url) async {
     print("Request: " + url);
 
     try {
-      dio.options.sendTimeout = 100;
-      dio.options.receiveTimeout = 100;
+      // dio.options.sendTimeout = 100;
+      // dio.options.receiveTimeout = 100;
 
       var response = await dio.get(url);
 
-      print("Response:::");
-      print("StatusCode : ${response.statusCode}");
-      print("----");
-      return;
+      handleUserDefinedError(response.data);
+
+      return Future.value(response.data);
     } catch (error) {
-      if (error is DioError) {
-        var errorDescription;
-        switch (error.type) {
-          case DioErrorType.CANCEL:
-            errorDescription = "Request to API server was cancelled";
-            break;
-          case DioErrorType.CONNECT_TIMEOUT:
-            errorDescription = "Connection timeout with API server";
-            break;
-          case DioErrorType.DEFAULT:
-            errorDescription =
-                "Connection to API server failed due to internet connection";
-            break;
-          case DioErrorType.RECEIVE_TIMEOUT:
-            errorDescription = "Receive timeout in connection with API server";
-            break;
-          case DioErrorType.SEND_TIMEOUT:
-            errorDescription = "Send timeout in connection with API server";
-            break;
-          case DioErrorType.RESPONSE:
-            errorDescription =
-                "Received invalid status code: ${error.response.statusCode}";
-            break;
-        }
-        print(errorDescription);
+      handleDioError(error);
+      return Future.value("ERROR");
+    }
+  }
+
+  handleUserDefinedError(responseData) {
+    print(":: UserDefined Error ::");
+    if (responseData["error"] == true) {
+      switch (responseData["error_code"]) {
+        case "BRANCHED_ENDPOINT":
+          print(responseData["message"]);
+          break;
+        default:
+          print("Undefined UserDefined Error");
+          print(responseData);
+          break;
       }
-      return;
+    }
+  }
+
+  handleDioError(error) {
+    print(":: Dio Error ::");
+    if (error is DioError) {
+      var errorDescription;
+      switch (error.type) {
+        case DioErrorType.CANCEL:
+          errorDescription = "Request to API server was cancelled";
+          break;
+        case DioErrorType.CONNECT_TIMEOUT:
+          errorDescription = "Connection timeout with API server";
+          break;
+        case DioErrorType.DEFAULT:
+          errorDescription =
+              "Connection to API server failed due to internet connection";
+          break;
+        case DioErrorType.RECEIVE_TIMEOUT:
+          errorDescription = "Receive timeout in connection with API server";
+          break;
+        case DioErrorType.SEND_TIMEOUT:
+          errorDescription = "Send timeout in connection with API server";
+          break;
+        case DioErrorType.RESPONSE:
+          errorDescription =
+              "Received invalid status code: ${error.response.statusCode}";
+          break;
+      }
+      print(errorDescription);
     }
   }
 }
